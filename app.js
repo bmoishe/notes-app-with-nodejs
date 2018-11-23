12a18e591dd3c316f725b6ff49f31205662d6cd4
bmoishe <moishebarnes@gmail.com>
2018-11-23 11:49:37 +0000

    test passing

--- app.js
+++ app.js
@@ -3,19 +3,7 @@ const app = express();
 const path = require("path");
 
 app.get('/', (req,res) => {
-  res.send(`
-     <html>
-     <title>
-       My awesome page
-     </title>
-     <body>
-     <div id="message">
-       Hello world!
-     </div>
-     </body>
-     </html>
-   `);
-  // res.sendFile(path.join(__dirname+"/index.html"));
+  res.sendFile(path.join(__dirname+"/index.html"));
 });
 
 app.listen(3003, () => console.log('Listening on port 3003'));
